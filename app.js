e32fd35fd1f2ea907c735162fcad2911ba0dd481
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-06-06 19:59:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import { mongoose } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import { verifyToken } from "./middlewares/Auth.js";
+import { userRouter } from "./routes/userRouter.js";
+import { taskRouter } from "./routes/taskRouter.js";
+import {
+  createTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateTask,
+} from "./controllers/taskControllers.js";
+
+const url = process.env.MONGO_DB;
+
+mongoose
+  .connect(url)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//Users
+app.use("/api/users",userRouter);
+
+//Tasks
+app.use("/api/tasks", verifyToken, taskRouter);
+
+app.use((req, res) => {
+  res
+    .status(404)
+    .json({ status: "error", message: "Not found this resources" });
+});
+
+export default app;
