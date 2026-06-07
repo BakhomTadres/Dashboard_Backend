0c1fbf024b8e9c11a68f5fa55247083d6a701de8
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-06-07 14:14:24 +0300

    Add files via upload

--- middlewares/LoginProtected.js
+++ middlewares/LoginProtected.js
@@ -1,14 +1,15 @@
 import JWT from "jsonwebtoken"
 export const LoginProtected = async (req, res, next) => {
     const authHeader = req.headers["Authorization"] || req.headers["authorization"];
+    if (!authHeader) return next();
+    
     const token = authHeader.split(" ")[1];
     if (!token) return next();
 
     try {
-        JWT.verify(token, process.env.TOKEN_SECRET_KEY)
-        return (res.status(400).json("You are already logged in"));
-    }
-    catch {
-        next();
+        JWT.verify(token, process.env.TOKEN_SECRET_KEY);
+        return res.status(400).json({ message: "You are already logged in" });
+    } catch {
+        next(); 
     }
 }
