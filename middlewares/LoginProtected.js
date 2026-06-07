58faa90688a53d866ea38771fbe6180dbe39b4d9
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-06-07 14:01:07 +0300

    Add files via upload

--- middlewares/LoginProtected.js
+++ middlewares/LoginProtected.js
@@ -1,10 +1,14 @@
+import JWT from "jsonwebtoken"
 export const LoginProtected = async (req, res, next) => {
     const authHeader = req.headers["Authorization"] || req.headers["authorization"];
-    if (!authHeader) return next();
     const token = authHeader.split(" ")[1];
-    if (token) {
-        return (res.status(400).json("You are login"));
-    }
+    if (!token) return next();
 
-    next();
+    try {
+        JWT.decode(token, process.env.TOKEN_SECRET_KEY)
+        return (res.status(400).json("You are already logged in"));
+    }
+    catch {
+        next();
+    }
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,7 +8,7 @@ import {
   logout,
 } from "../controllers/userControllers.js";
 import { verifyToken } from "../middlewares/Auth.js";
-import {LoginProtected} from "../middlewares/LoginProtected.js"
+import { LoginProtected } from "../middlewares/LoginProtected.js";
 export const userRouter = express.Router();
 
 userRouter.route("/").get(getUsers);
@@ -16,6 +16,6 @@ userRouter
   .route("/me")
   .get(verifyToken, getCurrentUser)
   .patch(verifyToken, updateUser);
-userRouter.route("/register").post(register);
-userRouter.route("/login").post(LoginProtected,login);
-userRouter.route("/logout").post(verifyToken,logout);
+userRouter.route("/register").post(LoginProtected, register);
+userRouter.route("/login").post(LoginProtected, login);
+userRouter.route("/logout").post(verifyToken, logout);
